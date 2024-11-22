6defcf71f0c74db587e29365523ff5c20612a890
tkp315 <114278931+tkp315@users.noreply.github.com>
2024-11-22 20:30:44 +0530

    checking for google id

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -3,7 +3,7 @@ import GoogleLogin from '../components/Auth/GoogleLogin';
 import PropType from 'prop-types'
 
 const GOOGLE_CLIENT_ID = import.meta.env.GOOGLE_CLIENT_ID
-
+console.log("Google Client Id",GOOGLE_CLIENT_ID)
 
 function AuthProvider({role}) {
   return (
